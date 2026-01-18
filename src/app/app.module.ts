a541b72ae3bbb7500052b997bffee1fe39f08152
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-01-18 15:24:22 +0100

    OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule
