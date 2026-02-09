dd0106804a22909efa14aefdb612f4e4ab6e94d8
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-02-09 13:02:37 +0100

    OK

---
+++ src/app/validators/lyrach-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { LyrachShopValidators } from './lyrach-shop-validators';
+
+describe('LyrachShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new LyrachShopValidators()).toBeTruthy();
+  });
+});
